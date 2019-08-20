21c22d6b6c71a1d478515ceebc5cd79ebf59eee0
Alumno <Alumno@PC16>
2019-08-20 09:46:19 -0500

    new

---
+++ ciclo/ciclo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ciclofor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Imprime los numeros del X hasta Y de -1");
+
+            Console.WriteLine("De donde arrancamos el ciclo (Mayor): ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("De donde Terminamos el ciclo (Menor): ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("De cuanto en cuanto: ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = x; i > y; i -= z)
+            {
+                Console.WriteLine("i = " + i);
+
+            }
+        }
+    }
+}
